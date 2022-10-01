ff5334816702c04b75273feada18ddb96907d142
Ilya <ilyaa@Ilyas-MacBook-Air.local>
2022-10-01 13:07:14 +0300

    Prototype #0.1

---
+++ lib_setup.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+
+#get image classifiers
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
